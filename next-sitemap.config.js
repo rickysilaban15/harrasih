8a8cbcb6c065f04535203a58bc5629b638ce53cd
Ricky Steven Silaban <rickysilaban384@gmail.com>
2025-08-08 17:26:43 +0700

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://harrasih.vercel.app', // ganti dengan domain kamu
+  generateRobotsTxt: true, // otomatis buat robots.txt
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
