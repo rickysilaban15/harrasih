4af2a3699551d638e777c60ae58c0d5420f42d26
Ricky Steven Silaban <rickysilaban384@gmail.com>
2025-08-08 19:39:17 +0700

    Add files via upload

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Harrasih - Portofolio & Pengembangan Web</title>
+      <meta name="description" content="Situs resmi Ricky Steven - Pengembangan aplikasi, web, dan solusi digital." />
+      <meta property="og:image" content="/images/preview.png" />
+    </>
+  );
+}
